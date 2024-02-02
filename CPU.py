a1d44a93831008e52087cc7a3652ba566f3dd938
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-01 18:27:35 -0700

    created CPU class and CPU test files, outlined some functions

---
+++ CPU.py
@@ -0,0 +1,9 @@
+class CPU:
+    def add():
+        pass
+    def subtract():
+        pass
+    def multiply():
+        pass
+    def divide():
+        pass
---
+++ test_CPU.py
@@ -0,0 +1,22 @@
+import pytest
+import CPU
+
+def test_add_success():
+    pass
+def test_add_fail():
+    pass
+
+def test_subtract_success():
+    pass
+def test_subtract_fail():
+    pass
+
+def test_multiply_success():
+    pass
+def test_multiply_fail():
+    pass
+
+def test_divide_success():
+    pass
+def test_divide_fail():
+    pass
