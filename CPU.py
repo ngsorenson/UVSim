32cc9751b3fe766de4912402a2c8dca49efa5625
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-02 21:51:24 -0700

    implemented parameter validation

--- CPU.py
+++ CPU.py
@@ -1,12 +1,22 @@
+from uvsim import UVSim
+
 class CPU:
     def ADD(self, operand, accumulator):
-        return accumulator + operand
+        if isinstance(accumulator, UVSim) and isinstance(operand, int):
+            return accumulator + operand
+        raise ValueError("Invalid parameter(s)")
 
     def SUBTRACT(self, operand, accumulator):
-        return accumulator - operand
-
+        if isinstance(accumulator, UVSim) and isinstance(operand, int):
+            return accumulator - operand
+        raise ValueError("Invalid parameter(s)")
+    
     def MULTIPLY(self, operand, accumulator):
-        return accumulator * operand
-
+        if isinstance(accumulator, UVSim) and isinstance(operand, int):
+            return accumulator * operand
+        raise ValueError("Invalid parameter(s)")
+    
     def DIVIDE(self, operand, accumulator):
-        return accumulator / operand
+        if isinstance(accumulator, UVSim) and isinstance(operand, int):
+            return accumulator // operand
+        raise ValueError("Invalid parameter(s)")
