deac9e5ff314515b394e3477cc70a80e6441691a
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-06 15:26:07 -0700

    failure tests now work properly

--- Milestone 2/test_CPU.py
+++ Milestone 2/test_CPU.py
@@ -1,35 +1,46 @@
 import pytest
-import CPU
-from UVSim import UVSim
+from CPU import CPU
+
 
 def test_add_success():
-    uvsim = UVSim()
-    uvsim.accumulator = 1010
-    assert uvsim.CPU.ADD(101, uvsim.accumulator) == 1111
+    cpu = CPU()
+    assert cpu.ADD(101, 1010) == 1111
+
+
 def test_add_fail():
-    uvsim = UVSim()
-    assert uvsim.CPU.ADD(101, 1010) == TypeError
+    cpu = CPU()
+    with pytest.raises(TypeError):
+        cpu.ADD(101, "1010")  # accumulator is not int
+
 
 def test_subtract_success():
-    uvsim = UVSim()
-    uvsim.accumulator = 1111
-    assert uvsim.CPU.SUBTRACT(101, uvsim.accumulator) == 1010
+    cpu = CPU()
+    assert cpu.SUBTRACT(101, 1111) == 1010
+
+
 def test_subtract_fail():
-    uvsim = UVSim()
-    assert uvsim.CPU.SUBTRACT(101, 1111) == TypeError
+    cpu = CPU()
+    with pytest.raises(TypeError):
+        cpu.SUBTRACT(101.1, 1111)  # value is not int
+
 
 def test_multiply_success():
-    uvsim = UVSim()
-    uvsim.accumulator = 1010
-    assert uvsim.CPU.MULTIPLY(5, uvsim.accumulator) == 5050
+    cpu = CPU()
+    assert cpu.MULTIPLY(5, 1010) == 5050
+
+
 def test_multiply_fail():
-    uvsim = UVSim()
-    assert uvsim.CPU.MULTIPLY(5, 1010) == TypeError
+    cpu = CPU()
+    with pytest.raises(TypeError):
+        cpu.MULTIPLY("5", 1010)  # value is not int
+
 
 def test_divide_success():
-    uvsim = UVSim()
-    uvsim.accumulator = 5050
-    assert uvsim.CPU.DIVIDE(5, uvsim.accumulator) == 1010
+    cpu = CPU()
+    assert cpu.DIVIDE(5, 5050) == 1010
+
+
 def test_divide_fail():
-    uvsim = UVSim()
-    assert uvsim.CPU.DIVIDE(5, 5050) == TypeError
+    cpu = CPU()
+    with pytest.raises(TypeError):
+        cpu.DIVIDE(5, 5050.3)  # accumulator is not int
