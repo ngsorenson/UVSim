deb106292fb96ffcae606ce206285a35ef097a99
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-06 15:25:28 -0700

    removed UVSim import to stop circular import, changed parameter validation to match

--- Milestone 2/CPU.py
+++ Milestone 2/CPU.py
@@ -1,5 +1,3 @@
-from uvsim import UVSim #imported for type checking accumulator
-
 class CPU:
     '''
     Pass in the value from UVSim or handle accessing memory to find 
@@ -8,21 +6,21 @@ class CPU:
     Will this work properly with UVSim class?
     '''
     def ADD(self, value, accumulator):
-        if isinstance(accumulator, UVSim) and isinstance(value, int):
+        if isinstance(accumulator, int) and isinstance(value, int):
             return accumulator + value
         raise TypeError("Invalid parameter type(s)")
 
     def SUBTRACT(self, value, accumulator):
-        if isinstance(accumulator, UVSim) and isinstance(value, int):
+        if isinstance(accumulator, int) and isinstance(value, int):
             return accumulator - value
         raise TypeError("Invalid parameter type(s)")
     
     def MULTIPLY(self, value, accumulator):
-        if isinstance(accumulator, UVSim) and isinstance(value, int):
+        if isinstance(accumulator, int) and isinstance(value, int):
             return accumulator * value
         raise TypeError("Invalid parameter type(s)")
     
     def DIVIDE(self, value, accumulator):
-        if isinstance(accumulator, UVSim) and isinstance(value, int):
+        if isinstance(accumulator, int) and isinstance(value, int):
             return accumulator // value
         raise TypeError("Invalid parameter type(s)")
