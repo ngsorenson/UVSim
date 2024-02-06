e96a2bb4fce60957f9a7d043a26100ee2354cd28
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-06 14:14:38 -0700

    implemented tests

--- Milestone 2/test_CPU.py
+++ Milestone 2/test_CPU.py
@@ -1,22 +1,35 @@
 import pytest
 import CPU
+from UVSim import UVSim
 
 def test_add_success():
-    pass
+    uvsim = UVSim()
+    uvsim.accumulator = 1010
+    assert uvsim.CPU.ADD(101, uvsim.accumulator) == 1111
 def test_add_fail():
-    pass
+    uvsim = UVSim()
+    assert uvsim.CPU.ADD(101, 1010) == TypeError
 
 def test_subtract_success():
-    pass
+    uvsim = UVSim()
+    uvsim.accumulator = 1111
+    assert uvsim.CPU.SUBTRACT(101, uvsim.accumulator) == 1010
 def test_subtract_fail():
-    pass
+    uvsim = UVSim()
+    assert uvsim.CPU.SUBTRACT(101, 1111) == TypeError
 
 def test_multiply_success():
-    pass
+    uvsim = UVSim()
+    uvsim.accumulator = 1010
+    assert uvsim.CPU.MULTIPLY(5, uvsim.accumulator) == 5050
 def test_multiply_fail():
-    pass
+    uvsim = UVSim()
+    assert uvsim.CPU.MULTIPLY(5, 1010) == TypeError
 
 def test_divide_success():
-    pass
+    uvsim = UVSim()
+    uvsim.accumulator = 5050
+    assert uvsim.CPU.DIVIDE(5, uvsim.accumulator) == 1010
 def test_divide_fail():
-    pass
+    uvsim = UVSim()
+    assert uvsim.CPU.DIVIDE(5, 5050) == TypeError
