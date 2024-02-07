8155a995e6f54842d7f8e386ed0fb2082efe5f63
Thomas Chappell <jthomaschappell@gmail.com>
2024-02-07 13:32:47 -0700

    Added value error checking

--- Milestone 2/memory.py
+++ Milestone 2/memory.py
@@ -10,75 +10,92 @@ class Memory:
 		self.memory_array = memory_array
 
 
-	def READ(self, location_int):
+	def READ(self, address):
 		"""Reads a word from the keyboard to somewhere in memory. 
 
 		Args:
-			location (_type_): _description_
+			address (_type_): Address to put the user input into in memory. 
 		"""
-		user_input = input(f"Input an integer value to go in memory location {location_int}: ")
-		while True:
-			try:
-				input_int = int(user_input)
-				self.memory_array[location_int] = input_int
-				break
-			except ValueError:
-				print("Input is not a valid integer.")
-		print(f"Success! {input_int} was added at memory location {location_int}. ")
+		try: 
+			address_int = int(address)
+			user_input = input(f"Input an integer value to go in memory address {address_int}: ")
+			while True:
+				try:
+					input_int = int(user_input)
+					self.memory_array[address_int] = input_int
+					break
+				except ValueError:
+					print("Input is not a valid integer.")
+			print(f"Success! {input_int} was added at memory address {address_int}. ")
+
+		except ValueError:
+			print("Error: Address value is not a valid integer")
 
 
 
 
-
-	def WRITE(self, location_int):
-		"""Takes a memory location (int) and prints the value at that location. 
+	def WRITE(self, address):
+		"""Takes a memory address (int) and prints the value at that address. 
 
 		Args:
-			location (_type_): input string
+			address (_type_): input string
 		"""
-		if location_int not in range(self.min, self.max + 1):
-			print(f"Error: Memory location {location_int} is not valid. It must be between {self.min} and {self.max}.")
-		else:
-			print(f"At memory location {location_int} is {self.memory_array[location_int]}")
+		try: 
+			address_int = int(address)
+			if address_int not in range(self.min, self.max + 1):
+				print(f"Error: Memory address {address_int} is not valid. It must be between {self.min} and {self.max}.")
+			else:
+				print(f"At memory address {address_int} is {self.memory_array[address_int]}")
+		except ValueError:
+			print("Error: Address value is not a valid integer")
 
 
 
-
-	def LOAD(self, location_int): 
-		"""If a valid location, returns the value at that location.
+	def LOAD(self, address): 
+		"""If a valid address, returns the value at that address.
 
 		Args:
-			location_int (_int_): The memory location in the array
+			address (_int_): The memory address in the array
 
 		Returns:
-			_int_: The value at that location
+			_int_: The value at that address
 		"""
+		try:
+			address_int = int(address)
 		# if address is not in the right range return None. 
-		if location_int not in range (self.min, self.max + 1):
-			print(f"Error: Memory location {location_int} is not valid. It must be between {self.min} and {self.max}.")
-		else:
-			value_at_location = self.memory_array[location_int]
-			print(f"At memory location {location_int} is {value_at_location}")
-			return value_at_location
+			if address not in range (self.min, self.max + 1):
+				print(f"Error: Memory address_int {address_int} is not valid. It must be between {self.min} and {self.max}.")
+			else:
+				value_at_address_int = self.memory_array[address_int]
+				print(f"At memory address {address_int} is {value_at_address_int}")
+				return value_at_address_int
+		except ValueError:
+			print("Error: Address value is not a valid integer")
 
 
 
 
-	def STORE(self, accumulator, location_int):
-		if location_int not in range (self.min, self.max + 1):
-			print(f"Error: Memory location {location_int} is not valid. It must be between {self.min} and {self.max}.")
-			print("Not added to memory.")
-		else:
-			# Error check if accumulator is not an int. 
-			try:
-				acc_int = int(accumulator)
-			except ValueError:
-				print("Error: Accumulator value is not an integer.")
+
+	def STORE(self, accumulator, address):
+		try:
+			address_int = int(address)
+			if address_int not in range (self.min, self.max + 1):
+				print(f"Error: Memory address_int {address_int} is not valid. It must be between {self.min} and {self.max}.")
 				print("Not added to memory.")
-			# take accumulator value and set as value at memory location int. 
-			self.memory_array[location_int] = acc_int
-			print("Added to memory!")
-			print(f"At memory location {location_int} is {acc_int}")
+			else:
+				# Error check if accumulator is not an int. 
+				try:
+					acc_int = int(accumulator)
+				except ValueError:
+					print("Error: Accumulator value is not an integer.")
+					print("Not added to memory.")
+				# take accumulator value and set as value at memory address_int int. 
+				self.memory_array[address_int] = acc_int
+				print("Added to memory!")
+				print(f"At memory address_int {address_int} is {acc_int}")
+		except ValueError:
+			print("Error: Address value is not a valid integer")
+
 
 			
 
@@ -100,11 +117,11 @@ def main():
 
 
 	# user_input = input("Integer to read in: ")
-	location1 = 20
-	memory.READ(location1)
+	address1 = 20
+	memory.READ(address1)
 
-	location2 = 99
-	memory.WRITE(location2)
+	address2 = 99
+	memory.WRITE(address2)
 
 
 
