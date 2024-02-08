f37ec3cdf2746c82fde0c9391615736317331d07
Tydidericksen <110498585+Tydidericksen@users.noreply.github.com>
2024-02-08 16:56:52 -0700

    Added 5 unit tests to the UVSim class
    These are just the first 5 I am planning on adding at least 2 more

--- Milestone 2/test_uvsim.py
+++ Milestone 2/test_uvsim.py
@@ -1,17 +1,52 @@
 import pytest
 from uvsim import UVSim
+import sys
 
-def test_write_success():
-    pass
+# Test load_program_into_memory using Test1.txt
+def test_load_program_into_memory_success():
+    """
+    Testing load_program_into_memory function
+    """
+    uvsim = UVSim()
+    uvsim.load_program_into_memory("testuvsim/Test1.txt")
+    assert uvsim.memory.LOAD(0) == 1007
 
-def test_write_fail():
-    pass
+    # test load_program_into_memory failure
+def test_load_program_into_memory_failure():
+    """
+    Testing load_program_into_memory function
+    """
+    uvsim = UVSim()
+    with pytest.raises(FileNotFoundError):
+        uvsim.load_program_into_memory("Failure.txt")
 
-def test_run_success_1():
-    pass
+# Test run_program using Test1.txt
+def test_run_program_success():
+    """
+    Testing run_program function
+    """
+    uvsim = UVSim()
+    uvsim.load_program_into_memory("testuvsim/Test1.txt")
+    uvsim.run_program()
+    assert uvsim.memory.LOAD(0) == 1007
 
-def test_run_success_2():
-    pass
+    # test run_program failure
+def test_run_program_failure():
+    """
+    Testing run_program function
+    """
+    uvsim = UVSim()
+    with pytest.raises(EOFError):
+        uvsim.run_program()
 
-def test_run_fail():
-    pass
+
+#test HALT instruction success
+def test_HALT_success():
+    """
+    Testing HALT instruction
+    """
+    uvsim = UVSim()
+    uvsim.memory.STORE(4300, 0)
+    uvsim.run_program()
+    assert uvsim.memory.LOAD(0) == 4300
+
