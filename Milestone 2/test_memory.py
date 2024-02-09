4a2241eb67de3406a86990487b4603750be841a2
Thomas Chappell <jthomaschappell@gmail.com>
2024-02-09 16:07:02 -0700

    finished half of the unit tests in test_memory.py

--- Milestone 2/test_memory.py
+++ Milestone 2/test_memory.py
@@ -44,11 +44,66 @@ def test_read_fail():
             memory.READ(address)
 
 
-def test_write_success():
-    pass
+def test_write_success(capfd):
+    memory = Memory()
+    address = 10
+    value = 1337
+    memory.STORE(value, address)
+    memory.WRITE(10)
+    captured = capfd.readouterr()
+    assert captured.out == f"At memory address {address} is {value}"
+    assert captured.err == ""
 
-def test_write_fail():
-    pass
+
+def test_write_fail_index():
+    # TODO: out of bounds. 
+    memory = Memory()
+    address = 1000
+    with pytest.raises(IndexError):
+        memory.WRITE(address)
+
+def test_write_fail_value():
+    memory = Memory()
+    address = "muffins"
+    with pytest.raises(ValueError):
+        memory.write(address)
+
+
+def test_store_success():
+    memory = Memory()
+
+    # before insertion
+    assert memory.memory_array[address] == None
+    assert memory.LOAD(address) == None
+    assert memory.memory_array[address+1] == None
+    assert memory.LOAD(address+1) == None
+
+
+    value = 42
+    address = 75
+    memory.STORE(value, address)
+
+    # after insertion
+    assert memory.memory_array[address] == value
+    assert memory.LOAD(address) == value
+    assert memory.memory_array[address+1] == None
+    assert memory.LOAD(address+1) == None
+
+    # TODO: Print the whole array to make sure no funny business happened. 
+
+def test_store_fail_index():
+    memory = Memory()
+    address = 1000
+    with pytest.raises(IndexError):
+        memory.WRITE(address)
+
+
+def test_store_fail_value():
+    memory = Memory()
+    address = "muffins"
+    with pytest.raises(ValueError):
+        memory.write(address)
+
 
 def test_load_success():
     pass
@@ -56,9 +111,3 @@ def test_load_success():
 def test_load_fail():
     pass
 
-
-def test_store_success():
-    pass
-
-def test_store_fail():
-    pass
