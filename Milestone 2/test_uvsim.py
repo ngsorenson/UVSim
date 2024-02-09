31d85152fdb1a678a71e5e159a18d7560ea03776
Tydidericksen <110498585+Tydidericksen@users.noreply.github.com>
2024-02-09 15:27:32 -0700

    Update test_uvsim.py
    Fixed the failed tests

--- Milestone 2/test_uvsim.py
+++ Milestone 2/test_uvsim.py
@@ -1,6 +1,8 @@
 import pytest
 from uvsim import UVSim
+from memory import Memory
 import sys
+
 
 # Test load_program_into_memory using Test1.txt
 def test_load_program_into_memory_success():
@@ -8,7 +10,7 @@ def test_load_program_into_memory_success():
     Testing load_program_into_memory function
     """
     uvsim = UVSim()
-    uvsim.load_program_into_memory("testuvsim/Test1.txt")
+    uvsim.load_program_into_memory("Test1.txt")
     assert uvsim.memory.LOAD(0) == 1007
 
     # test load_program_into_memory failure
@@ -20,33 +22,27 @@ def test_load_program_into_memory_failure():
     with pytest.raises(FileNotFoundError):
         uvsim.load_program_into_memory("Failure.txt")
 
-# Test run_program using Test1.txt
+#test run_program success
 def test_run_program_success():
     """
     Testing run_program function
-    """
-    uvsim = UVSim()
-    uvsim.load_program_into_memory("testuvsim/Test1.txt")
-    uvsim.run_program()
-    assert uvsim.memory.LOAD(0) == 1007
-
-    # test run_program failure
-def test_run_program_failure():
-    """
-    Testing run_program function
-    """
-    uvsim = UVSim()
-    with pytest.raises(EOFError):
-        uvsim.run_program()
-
-
-#test HALT instruction success
-def test_HALT_success():
-    """
-    Testing HALT instruction
     """
     uvsim = UVSim()
     uvsim.memory.STORE(4300, 0)
     uvsim.run_program()
     assert uvsim.memory.LOAD(0) == 4300
 
+#test run_program failure (shows how uvsim is handling the EOFError)
+class MockMemory(Memory):
+    def LOAD(self, address):
+        raise EOFError
+
+def test_run_program_failure():
+    """
+    Testing run_program function failure scenario
+    """
+    uvsim = UVSim()
+    uvsim.memory = MockMemory()
+
+    with pytest.raises(EOFError):
+        uvsim.run_program()
