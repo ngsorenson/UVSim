2f4217826ddfb7fb5b465bbe311184a5fa0ce2b5
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-08 18:38:10 -0700

    implemented main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from uvsim import UVSim
+
+
+def main():
+    file = sys.argv[1]
+
+    uvsim = UVSim()
+    print(uvsim.memory.memory_array)
+    uvsim.load_program_into_memory(file)
+    print(uvsim.memory.memory_array)
+    uvsim.run_program()
+    print(uvsim.memory.memory_array)
+
+if __name__=="__main__":
+    main()
+
