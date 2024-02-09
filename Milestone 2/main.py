f0d4b135f126adf7308a8127fddcfd77d07af05a
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-08 18:50:50 -0700

    actually commented out testing lines

--- Milestone 2/main.py
+++ Milestone 2/main.py
@@ -6,11 +6,11 @@ def main():
     file = sys.argv[1]
 
     uvsim = UVSim()
-    print(uvsim.memory.memory_array)
+    #print(uvsim.memory.memory_array)
     uvsim.load_program_into_memory(file)
-    print(uvsim.memory.memory_array)
+    #print(uvsim.memory.memory_array)
     uvsim.run_program()
-    print(uvsim.memory.memory_array)
+    #print(uvsim.memory.memory_array)
 
 if __name__=="__main__":
     main()
