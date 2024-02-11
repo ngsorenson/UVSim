548be03c581347c07c835967382435b49d810a01
Noah Sorenson <60499301+ngsorenson@users.noreply.github.com>
2024-02-11 14:00:21 -0700

    added overflow handling by truncating digits past 4

--- Milestone 2/CPU.py
+++ Milestone 2/CPU.py
@@ -2,25 +2,42 @@ class CPU:
     def ADD(self, accumulator, value):
         '''returns accumulator + value if both are ints'''
         if isinstance(accumulator, int) and isinstance(value, int):
-            return accumulator + value
+            output = accumulator + value
+            while abs(output) > 9999:
+                output = output // 10
+                print("Overflow on operation: truncating")
+            return output
+
         raise TypeError("Invalid parameter type(s)")
 
     def SUBTRACT(self, accumulator, value):
         '''returns accumulator - value if both are ints'''
         if isinstance(accumulator, int) and isinstance(value, int):
-            return accumulator - value
+            output = accumulator - value
+            while abs(output) > 9999:
+                output = output // 10
+                print("Overflow on operation: truncating")
+            return output
         raise TypeError("Invalid parameter type(s)")
     
     def MULTIPLY(self, accumulator, value):
         '''returns accumulator * value if both are ints'''
         if isinstance(accumulator, int) and isinstance(value, int):
-            return accumulator * value
+            output = accumulator * value
+            while abs(output) > 9999:
+                output = output // 10
+                print("Overflow on operation: truncating")
+            return output
         raise TypeError("Invalid parameter type(s)")
     
     def DIVIDE(self, accumulator, value):
-        '''returns accumulator // value if both are ints (truncated so that return value is an int)'''
+        '''returns accumulator // value if both are ints (truncated so that output = value is an int)'''
         if isinstance(accumulator, int) and isinstance(value, int):
             if value != 0:
-                return accumulator // value
+                output = accumulator // value
+                while abs(output) > 9999:
+                    output = output // 10
+                    print("Overflow on operation: truncating")
+                return output
             raise ZeroDivisionError
         raise TypeError("Invalid parameter type(s)")
