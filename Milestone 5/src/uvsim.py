7a7c096597d38c1af2ff368adeb4ac3d8209873b
Tate Thomas <106632630+TateThomas@users.noreply.github.com>
2024-04-12 17:52:34 -0600

    Refactoring and documentation for UVSim
    Mostly updated/added docstrings, added input validation to everything, and made a function _set_components to reduce duplicate code

--- Milestone 5/src/uvsim.py
+++ Milestone 5/src/uvsim.py
@@ -4,9 +4,28 @@ from program import Program, FourBitProgram, SixBitProgram
 import tkinter.simpledialog
 
 class UVSim:
-    """passing anything into the constructor for gui will change
-    the behavior of some functions to allow them to work with gui, 
-    self.output is a list of outputs returned py run_program"""
+    """A simple virtual machine that emulates a hypothetical machine language, 'BasicML'.
+    
+    Attributes:
+        program_versions (_dict_{_int_: _Program_}): A class attribute that keeps track of what UVSim version corresponds to what program class
+        version (_int_): An attribute letting UVSim and other outside sources know what version UVSim is currently running in
+        gui (_bool_): A flag that lets UVSim know that a GUI is connected so it can interact with it during program execution
+        program_class (_class_): An attribute holding the class to base programs on, correlated with what the current UVSim version is
+        current_program (_Program_ or None): The current program in UVSim that is represented in a universal format
+        memory (_Memory_): An instance of the memory class, a core component of UVSim
+        cpu (_CPU_): An instance of the CPU class, a core component of UVSim
+        accumulator (_int_ or None): A register that holds an arbitrary word, which can be changed or used for various operations during program execution
+        address (_int_ or None): A register that holds a word representing a memory address for some operations to use
+        program_counter (_int_): A register that keeps track of where program execution is in memory throughout program execution. It is always located 1 address before the next line to be executed
+        is_running (_bool_): A boolean that keeps track of if a program is currently running
+        output (_string_ or None): A register to write program output, where UVSim and a GUI communicate
+    
+    Methods:
+        change_version (new_version=_int_): Changes the UVSim version and reformats the current program
+        store_program_in_memory (file_lines=_list_[_str_], starting_line=_int_, skip_identification=_bool_): Stores a given program (represented as a list of strings) in memory
+        run_program (start_location=_int_ or None): Either starts or continues program execution and runs until a halt, crash, or EOF flag is detected
+        step_program (start_location=_int_ or None): Runs the next line in program execution
+    """
 
     # versions of UVSim corresponding to different program classes
     program_versions = {
@@ -15,45 +34,95 @@ class UVSim:
     }
 
     def __init__(self, version = SixBitProgram.UV_SIM_VERSION, gui = False):
+        """
+        Args:
+            version (_int_): Version to instantiate UVSim in
+            gui (_bool_): A flag that lets UVSim know that a GUI is connected so it can interact with it during program execution
+        """
+    
+        if not isinstance(version, int):
+            raise TypeError(f"UVSim version must be an integer, not {type(version)}")
+        if version not in UVSim.program_versions.keys():
+            raise ValueError(f"UVSim Version {version} does not exist (can be one of the following: {UVSim.program_versions.keys()})")
+        self.version = version
 
-        self.gui = gui                  # is there a gui
+        if not isinstance(gui, bool):
+            raise TypeError("GUI flag must be a boolean")
+        self.gui = gui
+
+        self.program_class = UVSim.program_versions[self.version]       # program type class
+        self.current_program = None      # current instance of the current program class
+        self._set_components()
 
         self.accumulator = 0             # current word
         self.op_code = None              # current instruction
         self.address = None              # current address for instruction
         self.program_counter = 0         # current address in program
         self.is_running = False          # is a program currently running
-        self.output = None
+        self.output = None               # holds program output
     
-        self.version = version          # uvsim version
-        self.program_class = UVSim.program_versions[self.version]       # program type class
+    def _set_components(self):
+        """Sets/resets UVSim memory and cpu.
+        
+        Args:
+            None
+        
+        Returns:
+            None
+        """
         self.memory = Memory(self.program_class.MAX_PROGRAM_LENGTH)     # memory instance
-        self.cpu = CPU(10**self.program_class.MAX_LINE_LENGTH - 1)      # cpu instance 
-
-        self.current_program = None     # current instance of the current program class
+        self.cpu = CPU(10**self.program_class.MAX_LINE_LENGTH - 1)      # cpu instance
 
     def change_version(self, new_version):
-
-        if not new_version in UVSim.program_versions.keys():
-            raise ValueError(f"Version {new_version} does not exist")
+        """Changes the UVSim version and reformats the current program (locally).
         
+        Args:
+            new_version (_int_): Version number to switch to
+        
+        Returns:
+            None
+        """
+
+        if not isinstance(new_version, int):
+            raise TypeError(f"New version must be an integer, not {type(new_version)}")
+        if new_version not in UVSim.program_versions.keys():
+            raise ValueError(f"UVSim Version {new_version} does not exist (can be one of the following: {UVSim.program_versions.keys()})")
         if new_version == self.version:
             return
         
         self.version = new_version
         self.program_class = UVSim.program_versions[self.version]
-
-        self.memory = Memory(self.program_class.MAX_PROGRAM_LENGTH)     # update memory instance
-        self.cpu = CPU(10**self.program_class.MAX_LINE_LENGTH - 1)      # update cpu instance 
+        self._set_components()
 
         if self.current_program is not None:
             self.current_program = self.program_class(self.current_program)
             self.memory.import_program(self.current_program)
 
     def store_program_in_memory(self, file_lines, starting_line = 0, skip_identification = False):
-        """ Writes data from file (specified by file_name) starting at memory address 0. """
+        """Stores a program represented as a list of strings starting at a given line in memory.
+        
+        Args:
+            file_lines (_list_[_str_]): A list of strings representing a program (it is best to use a FileFormatter class to do this for you to ensure correct formatting)
+            starting_line (_int_): Line in memory where the start of the program is stored (defaults to 0)
+            skip_identification (_bool_): Optional parameter to skip the identification of the program type of the given file and the request to change version if True (defauts to False)
+        
+        Returns:
+            None
+        """
 
         self.is_running = False
+
+        if not isinstance(file_lines, list):
+            raise TypeError(f"File lines must be a list of strings, not {type(file_lines)}")
+        if len(file_lines) >= self.memory.max:
+            raise ValueError(f"Program length exceeds the maximum length ({len(file_lines)} was given, max is {self.memory.max})")
+        if not isinstance(starting_line, int):
+            raise TypeError(f"Starting line must be an integer, not {type(starting_line)}")
+        if (starting_line < 0) or (starting_line >= self.memory.max):
+            raise ValueError(f"Starting line is not a valid address in memory ({starting_line} was given)")
+        if not isinstance(skip_identification, bool):
+            raise TypeError(f"Skip identification must be a boolean, not {type(skip_identification)}")
+        
         if not skip_identification:
             identified_type = Program.program_type(file_lines)
             if identified_type is None:
@@ -63,35 +132,67 @@ class UVSim:
                 if user_input:
                     self.program_class = identified_type
                     self.version = self.program_class.UV_SIM_VERSION
+        
         self.current_program = self.program_class(file_lines, starting_line)
         self.memory.import_program(self.current_program)
 
-    def run_program(self, start_location = 0):
-        """ Runs program starting at a given memory address (defaults to 0). """
+    def run_program(self, start_location = None):
+        """Either starts or continues program execution and runs until a halt, crash, or EOF flag is detected.
         
+        Args:
+            start_location (_int_ or None): Location in memory where to begin program execution (does not have an effect if program is already running), defaults to start of program
+        
+        Returns:
+            None
+        """
+        
+        if start_location is None:
+            start_location = self.current_program.start_location
+        if not isinstance(start_location, int):
+            raise TypeError(f"Start location must be an integer, not {type(start_location)}")
+        if (start_location < self.current_program.start_location) or (start_location >= self.current_program.end_location):
+            raise ValueError(f"Start location is outside of program bounds ({start_location} was given)")
+
         self.step_program(start_location)   # note: this line is neccessary to make sure self.is_running is true before starting the while loop (still works as expected when halt is reached on this line)
         while self.is_running:
             self.step_program()
                 
-    def step_program(self, start_location = 0):
+    def step_program(self, start_location = None):
+        """Runs the next line in program execution.
+        
+        Args:
+            start_location (_int_ or None): Location to begin program execution if program is not currently running (does not have an effect if program is already running), defaults to start of program
+        
+        Returns:
+            None
+        """
 
+        if start_location is None:
+            start_location = self.current_program.start_location
+        if not isinstance(start_location, int):
+            raise TypeError(f"Start location must be an integer, not {type(start_location)}")
+        if (start_location < self.current_program.start_location) or (start_location >= self.current_program.end_location):
+            raise ValueError(f"Start location is outside of program bounds ({start_location} was given)")
+
+        # reset attributes before starting program execution
         if not self.is_running:
             self.accumulator = 0
             self.output = None
             self.program_counter = start_location - 1
             self.is_running = True
 
+        # try catch block is here to stop program execution when an error is raised
         try:
             self.program_counter += 1
             current_line = self.memory.LOAD(self.program_counter)
 
             if current_line is None:
                 raise SyntaxError(f"Line {self.program_counter} cannot be read because it is empty")
-            if current_line == self.program_class.EOF_FLAG():   # check for end of file value
+            if current_line == self.program_class.EOF_FLAG():
                 raise EOFError("End of file flag reached")
         
-            self.op_code = self.program_class.get_op_code(current_line)  # extracts first 2 digits from current line
-            self.address = self.program_class.get_address(current_line)  # extracts last 2 digits from current line
+            self.op_code = self.program_class.get_op_code(current_line)
+            self.address = self.program_class.get_address(current_line)
 
             match self.op_code:
                 case 10:
